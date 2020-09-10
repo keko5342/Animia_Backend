17b8d7d806cb82a533685e4a7e348d3f0f498e17
keko5342 <kekosute@gmail.com>
2020-09-10 09:47:19 +0000

    code organization

--- config.js
+++ config.js
@@ -1,27 +1,15 @@
-const providers = ['twitter']
-
-// コールバック先URL
-const callbacks = providers.map(provider => {
-  return process.env.NODE_ENV === 'production'
-    ? `http://localhost:18080/${provider}/callback`
-    : `http://18.180.4.93:18080/${provider}/callback`
-})
-
-const [twitterURL] = callbacks
-
 // 想定されるクライアントサーバのドメイン
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? ['http://172.18.0.2:8080', 'http://172.24.0.2:5000']
-  : ['https://dev.da1sjddf4h4dx.amplifyapp.com', 'http://localhost:5000']
-//  : ['https://dev.da1sjddf4h4dx.amplifyapp.com', 'https://dev.da1sjddf4h4dx.amplifyapp.com']
+  ? [process.env.CLIENT_DOMAIN_PRODUCTION1, process.env.CLIENT_DOMAIN_PRODUCTION2]
+  : [process.env.CLIENT_DOMAIN_DEVELOP1, process.env.CLIENT_DOMAIN_DEVELOP2]
 
 // 認証に必要な情報の読み込み
 exports.TWITTER_CONFIG = {
   consumerKey: process.env.TWITTER_KEY,
   consumerSecret: process.env.TWITTER_SECRET,
-  callbackURL: twitterURL,
 }
 
+// DBの設定
 exports.DB_CONFIG = {
   host: process.env.DB_CONFIG_HOST,
   user: process.env.DB_CONFIG_USER,
@@ -29,6 +17,7 @@ exports.DB_CONFIG = {
   database: process.env.DB_CONFIG_DATABASE,
 }
 
+// AUTH0への認証に必要な情報
 exports.AUTH0_CONFIG = {
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 require('dotenv').config();                                   //.envファイルから設定の読み出し
 const express = require('express');                           //nodeフレームワーク
-const path = require('path');                                 //標準ファイル読み込み
 const fs = require('fs');                                     //標準ファイル書き込み
 const https = require('https');
 const http = require('http');
-const passport = require('passport');                         //API連携（今回はTwitter）
 const cors = require('cors');                                 //クロスオリジンアクセス
-const authRouter = require('./lib/auth.router');              //ルーティング
-const passportInit = require('./lib/passport.init');          //パスポート設定
-const { SESSION_SECRET, CLIENT_ORIGIN } = require('./config');//各種設定の読み出し
-const cookieParser = require('cookie-parser');                 //Cookieを解析
+const authRouter = require('./lib/router');              //ルーティング
+const { CLIENT_ORIGIN } = require('./config');//各種設定の読み出し
 
 const app = express()
 
@@ -27,17 +23,6 @@ server = https.createServer(certOptions, app)
 app.use(express.json());                          // JSONサポート
 app.use(express.urlencoded({ extended: true }));  // Urlエンコ
 app.use(cors({origin: CLIENT_ORIGIN, credentials: true, samesite: 'none', secure: true}));           // WebClientServerからのCORS通信のみ許可
-app.use(passport.initialize());                   // APIリクエスト対応
-passportInit();
-
-/*
-// セッション情報の設定
-app.use(session({ 
-  secret: process.env.SESSION_SECRET, 
-  resave: true, 
-  saveUninitialized: true
-}));
-*/
 
 // 起動確認
 app.get('/wake-up', (req, res) => res.send('👍'));
